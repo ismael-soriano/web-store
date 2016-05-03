b14d35ace6f06bd67c27957aed12fc57ac4727c1
ismael-soriano <luzaoban@gmail.com>
2016-05-03 17:29:49 +0200

    Added Autofac

---
+++ WebStoreWcf/Configurations/AutofacConfig.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using Domain;
+
+namespace WebStoreWcf.Configurations
+{
+    public class AutofacConfig
+    {
+        public static IContainer GetContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            // Register your service implementations.
+            builder.RegisterType<InvoiceService>().As<IInvoiceService>();
+            builder.RegisterType<ProductService>().As<IProductService>();
+            builder.RegisterType<ShopDbContext>().As<IRepositoryInvoices>();
+            builder.RegisterType<ShopDbContext>().As <IRepositoryProducts>();
+
+            // Set the dependency resolver.
+            return builder.Build();
+        }
+    }
+}
